cab3dbd60218f2d2406e35a8a1cfa4a61fe6c76b
Jon Desiderio <desiderj@uci.edu>
2015-01-14 21:02:22 -0800

    Test Scene
    Made temp. game objects.

---
+++ UntitledSidescroller/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+	//list of platforms
+	public static List<GameObject> platform_list;
+	// Use this for initialization
+	void Start () {
+		platform_list = new List<GameObject> ();
+			AddAllPlatforms ();
+		Debug.Log (platform_list);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void AddAllPlatforms(){
+		GameObject[] go = GameObject.FindGameObjectsWithTag ("Platform");
+			foreach(GameObject platform in go)
+				AddPlatform(platform.gameObject);
+	}
+	public void AddPlatform(GameObject platform){
+		platform_list.Add (platform);
+	}
+}
