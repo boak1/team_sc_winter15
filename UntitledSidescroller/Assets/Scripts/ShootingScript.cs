7d908d6b0ca789d5a193b6966f482df6876719ea
alexal9 <alexal9@uci.edu>
2015-01-15 18:54:42 -0800

    LAZERZ stuffz

--- UntitledSidescroller/Assets/Scripts/ShootingScript.cs
+++ UntitledSidescroller/Assets/Scripts/ShootingScript.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class ShootingScript : MonoBehaviour {
 
+	public GameObject blueLaser; 
+	private GameObject beam;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,18 @@ public class ShootingScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.J)) 
+		{
+			beam = (GameObject)Instantiate(blueLaser, transform.position+new Vector3(0, 0, 0), Quaternion.identity);
+		}
+		else if (Input.GetKeyDown(KeyCode.K)) 
+		{
+
+		}
+		else if (Input.GetKeyDown(KeyCode.L)) 
+		{
+			
+		}
+
 	}
 }
