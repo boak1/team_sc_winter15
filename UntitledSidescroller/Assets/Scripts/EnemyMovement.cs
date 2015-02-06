cf5c67b8777bdd87a3409358f9213ee42119d140
kgwong <wong.kevin47@gmail.com>
2015-02-05 18:18:25 -0800

    Basic Enemy

---
+++ UntitledSidescroller/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+	}
+}
