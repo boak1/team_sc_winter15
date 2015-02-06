2e94600b1fbf4adb74543e6ff6fac4ae24911b7e
Vincent Chang <vincentchang9999@gmail.com>
2015-02-05 19:33:19 -0800

    Created a global sfx player and added some temp sfx
    Made a new game object and a script
    
    Right now I have some test code running that plays sound effects if you
    press U or I during the game.

---
+++ UntitledSidescroller/Assets/Scripts/SfxPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AudioSource))]
+public class SfxPlayer : MonoBehaviour {
+
+    public AudioSource audioSource;
+
+    private Dictionary<string, AudioClip> soundDict = new Dictionary<string, AudioClip>();
+    public List<AudioClip> soundEffects = new List<AudioClip>();
+
+	// Use this for initialization
+	void Start () {
+
+        // Set up the dictionary from the list of sound effects
+        foreach (AudioClip clip in soundEffects)
+        {
+            soundDict.Add(clip.name, clip);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Test Code
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            PlaySfx("chargeup");
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            PlaySfx("teleport");
+        }
+	}
+
+    // When called, loads the specified sound effect into the AudioSource and plays it
+    void PlaySfx(string sfxName)
+    {
+        AudioClip clip = soundDict[sfxName];
+        audioSource.PlayOneShot(clip, 0.7f);
+    }
+}
