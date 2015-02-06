cc9bb6b8396b967ee11409f311cc065c710cb7a0
kgwong <wong.kevin47@gmail.com>
2015-02-05 19:04:46 -0800

    More basic enemy

--- UntitledSidescroller/Assets/Scripts/Health.cs
+++ UntitledSidescroller/Assets/Scripts/Health.cs
@@ -7,11 +7,27 @@ public class Health : MonoBehaviour {
     public int color; 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void setColor(int newColor)
+    {
+        switch (newColor)
+        {
+            case 1:
+                return;
+            case 2:
+                return;
+            case 3:
+                return;
+            default:
+                return;
+                
+        }
+    }
 }
