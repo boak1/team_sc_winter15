e844b3771b6a871b81264d31e5e559d53c58156d
eaho1 <eaho1@uci.edu>
2015-03-02 19:01:28 -0800

    Added a health sprite

---
+++ UntitledSidescroller/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private int hp = 3;
+    //get the hp sprites
+    public Sprite fullHp;
+    public Sprite oneHP;
+    public Sprite twoHP;
+    //used to change sprites
+    private SpriteRenderer spriteRendererH;    
+    void Start()
+    {
+
+        spriteRendererH = GetComponent<SpriteRenderer>(); //  the SpriteRenderer that is attached to the Gameobject
+        if (spriteRendererH.sprite == null) // if the sprite  is empty// set the sprite to fullHP
+            spriteRendererH.sprite = fullHp;
+
+    }
+    
+    void Update()
+    {
+        int hpTest = hp;
+
+        if (hpTest == 2)
+        {
+            spriteRendererH.sprite = twoHP;//change the sprite to 2 hp
+        }
+        else if (hpTest == 1)
+        {
+            spriteRendererH.sprite = oneHP; // change the sprite to 1 hp
+        }
+        else if (hpTest == 0)
+        {
+            Application.LoadLevel("Game Over"); //moves to game over screen
+
+        }
+    }
+}
