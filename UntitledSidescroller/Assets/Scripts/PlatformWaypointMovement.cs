8356b3342a4eec33b4811e3728fa69e75d3fb132
boak1 <boak@uci.edu>
2015-04-17 20:41:18 -0700

    moving platforms and new test scene

--- UntitledSidescroller/Assets/Scripts/PlatformWaypointMovement.cs
+++ UntitledSidescroller/Assets/Scripts/PlatformWaypointMovement.cs
@@ -3,32 +3,31 @@ using System.Collections;
 
 public class PlatformWaypointMovement : MonoBehaviour {
 	public Transform[] waypoints;
-	public float speed;
+	public float speed; //how fast the platform moves
+	public bool stopAtEnd = false; // if set to true in unity it moves till it gets to the last waypoint then stops
+	private bool stop = false;// if true platform stops
 	int index = 0;
 	
 	
 	void Update()
 	{
-		float step = speed * Time.deltaTime;        
+		float step = speed * Time.deltaTime;    //how much the platform moves each update
+
+		//v move the platform by one step if its appropriatev
+		if(!stop)
 		transform.position = Vector3.MoveTowards(transform.position, waypoints[index].position, step);
-		if (transform.position - waypoints[index].position == new Vector3(0,0,0))
+
+		//if the platform has hit a way point
+		if (transform.position - waypoints[index].position == new Vector3(0,0,0) && !stop)
 		{
-			index++;
-			if (index > waypoints.Length - 1)
+			index++;// go to the next waypoint
+
+			//should the platform stop or go back to the begining?
+			if (index > waypoints.Length - 1 && !stopAtEnd)
 				index = 0;
+			else if(index > waypoints.Length - 1)//if the platform should stop, stop checking its position
+				stop = true;
 		}            
 	}
-	
-	//void OnCollisionEnter2D(Collision2D hit)
-	//{
-	//    if (hit.gameObject.name.Contains("Waypoint"))            
-	//    {
-	//        Debug.Log("Hello");
-	//        index++;
-	//        if (index > waypoints.Length - 1)
-	//        {
-	//            index = 0;
-	//        }
-	//    }
-	//}
+
 }
