ecce737963620897ee5524a93debc8241b4adcaf
Vincent Chang <vincentchang9999@gmail.com>
2015-04-23 18:19:31 -0700

    Revert "Added player damage sound to Pirate Mountain"
    This reverts commit da99d0c783efd5dcc1ddafa1013e47738039b5e1.

--- UntitledSidescroller/Assets/Scripts/EnemyPreferences.cs
+++ UntitledSidescroller/Assets/Scripts/EnemyPreferences.cs
@@ -18,14 +18,11 @@ public class EnemyPreferences : MonoBehaviour {
 	public bool killOffScreen = true;
 	public bool givesHp, diesOnContact = true;
 	public int contactDamage = 1, hp4Kill = 1, enemyHP = 1;
-    ShootingScript SS;
-
-    public SfxPlayer sfxplayer;
+    ShootingScript SS; 
 
     void Start()
     {
         SS = GameObject.Find("PlayerShooting").GetComponent<ShootingScript>();  //Import ShootingScript
-        sfxplayer = GameObject.Find("SfxPlayer").GetComponent<SfxPlayer>();
     }
     void OnBecameVisible()
     {
@@ -49,7 +46,6 @@ public class EnemyPreferences : MonoBehaviour {
     {
 			if (hit.gameObject.name == "Player") {
 						PlayerHealth.hp -= contactDamage;
-                        sfxplayer.PlaySfx("Hit_Hurt_loud");
 						if (diesOnContact)
 								Destroy (this.gameObject);
 				}
