eab74ee179e4662a505843bd940b1a04acad3965
kgwong <wong.kevin47@gmail.com>
2015-05-08 18:20:43 -0700

    Added EnemyBecomesPlatformOnDeathBehavior script

---
+++ UntitledSidescroller/Assets/Scripts/EnemyBecomesPlatformOnDeathBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBecomesPlatformOnDeathBehavior : MonoBehaviour {
+
+    public GameObject platform;
+
+    void OnDestroy()
+    {
+        Instantiate(platform, transform.position, Quaternion.identity);
+    }
+}
