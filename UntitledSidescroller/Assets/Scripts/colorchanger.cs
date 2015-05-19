46a6808c064ab8cb810c18d2df58f347fc1762ae
warstark <smacha@uci.edu>
2015-05-19 01:59:59 -0700

    colorchanger

---
+++ UntitledSidescroller/Assets/Scripts/colorchanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorchanger : MonoBehaviour {
+	
+	Color[] colors=new Color[3];
+	// Use this for initialization
+	void Start () {
+		
+		colors [0] = Color.red;
+		colors [1] = Color.blue;
+		colors [2] = Color.green;
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider other) {
+		gameObject.GetComponent<Renderer>().material.color = colors [Random.Range (0, ColorSpace.Length)];
+		
+	}
+}
